2dba58ef256e5cae7160ce089351e9d128d2ccb4
Georg Bakken Idland <gebi@dsb.dk>
2021-07-21 12:55:44 +0200

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3007ee00ecd954132373128b78b3e7db'),
+    },
+  },
+});
